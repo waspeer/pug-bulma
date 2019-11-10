88fb7c41823c5a3754a423cfbc2bc9231fb1041b
Wannes Salomé <mail@wannessalome.nl>
2019-11-10 01:12:38 +0100

    added content element

---
+++ tests/elements/content.js
@@ -0,0 +1,16 @@
+import commonTests from "../_commonTests";
+import sizeTests from "../_sizeTests";
+
+const contentContext = {
+  name: "content",
+  mixinPath: "/elements/content",
+  types: [
+    {
+      type: "generic",
+      expectedTpl: '<div class="content">{{BLOCK}}</div>'
+    }
+  ]
+};
+
+commonTests(contentContext);
+sizeTests(contentContext);
